a27f674134cf8def2e9fc288b903322e8dbdb57c
Akash Tiwari <drakashti@gmail.com>
2017-09-03 20:51:21 +0530

    Update java_Applet.java

--- java_Applet.java
+++ java_Applet.java
@@ -3,20 +3,20 @@
  +            
  +  *  Here you can also explicitly create a Smiley.html file,if you don't want it in your java code.
  +*/
- +
- +import java.applet.*;
- +import java.awt.Graphics;
- +
- +/*
- +<applet code="Smiley.class" width=200 height=150>
- +</applet>
- +*/
- +
- +public class Smiley extends Applet {
- +    public void paint(Graphics g) {
- +        g.drawOval(10,10,40,15);
- +        g.drawOval(90,10,40,15);
- +        g.drawLine(70,20,70,50);
- +        g.drawArc(40,50,50,20,180,180);
- +    }
- +}
+ 
+ import java.applet.*;
+ import java.awt.Graphics;
+ 
+ /*
+ <applet code="Smiley.class" width=200 height=150>
+ </applet>
+ */
+ 
+ public class Smiley extends Applet {
+     public void paint(Graphics g) {
+         g.drawOval(10,10,40,15);
+         g.drawOval(90,10,40,15);
+         g.drawLine(70,20,70,50);
+         g.drawArc(40,50,50,20,180,180);
+     }
+ }
