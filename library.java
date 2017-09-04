50edd0f5c26fcb8bab7450f31391d6b08c5e1e33
Akash Tiwari <drakashti@gmail.com>
2017-09-04 19:00:47 +0530

    Create library.java

---
+++ library.java
@@ -0,0 +1,69 @@
+
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Library {
+    public static void main(String[] args) {
+  Book b=new Book();
+    }
+    
+}                                              
+
+public class Book
+{
+String bk_name;
+float price;
+char author;
+String availability;
+Scanner s= new Scanner(System.in);
+public Book(){}
+Book(String bkn,float p,char author,String availability)
+{
+bk_name=bkn;
+price=p;
+this.author=author;
+this.availability=availability;
+}
+
+void librarian(){
+
+int it,rt,fine,n;
+System.out.println("Enter the issue _date and return_date");
+it=s.nextInt();
+rt=s.nextInt();
+n=it-rt;
+if(n>7)
+{   fine=(n-7)*5;
+  System.out.println("The amount as fine is:"+fine);
+}
+else 
+    System.out.println("NO Fine");
+}
+ 
+void student()
+{
+String name,sclass;
+int roll;
+System.out.println("enter your name,class,roll no: ");
+name=s.next();
+sclass=s.next();
+roll=s.nextInt();
+List<String> list = new ArrayList<>();
+list.add(new Book("oopm","samira",200,"YES"));
+list.add(new Book("dbms","TECHMAX",300,"YES"));
+list.add(new Book("dsa","TACHMAX",150,"YES"));
+list.add(new Book("adc","TECHMAX",400,"YES"));
+list.add(new Book("padc","TECHMAX",145,"YES"));
+
+Scanner input=new Scanner(System.in);
+System.out.println("Enter book name:");
+String st=input.nextLine();
+ 
+for (Book s : list){
+if(st.equals(s.bk_name)){
+System.out.println("\n book_name:"+bk_name +"\nAuthor:"+ author +"\n Price:"+ price +"\n Availability:"+availability);
+}   }
+}
+}
