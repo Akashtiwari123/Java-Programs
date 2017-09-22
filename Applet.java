a39d6a2d1814a2d84895c3505c76152e5605928d
Akash Tiwari <drakashti@gmail.com>
2017-09-22 23:11:52 +0530

    Create Applet.java

---
+++ Applet.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+public class first extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawRect(80,80,20,40);
+g.setColor(Color.RED);
+g.drawOval(40,40,100,200);
+g.fillOval(68,81,10,20);
+g.setColor(Color.BLUE);
+g.fillArc(120,130,22,10,20,30);
+}
+} 
