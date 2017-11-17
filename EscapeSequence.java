c6a996354b03d110451f8bb025d89636f236504e
Akash Tiwari <drakashti@gmail.com>
2017-11-17 11:15:43 +0530

    Create EscapeSequence.java

---
+++ EscapeSequence.java
@@ -0,0 +1,17 @@
+/*Write a program to demonstrate the use of escape characters / sequences and println
+statements.
+SOURCE CODE:*/
+import java.io.*;
+public class Harry
+{
+public static void main(String args[])
+{
+System.out.println("\tI thought 'you' were my Angel,");
+System.out.println("\t\tBut Angel never lie.");
+System.out.println("\t\t\"\\\\//\\\\//\"");
+System.out.println("\tI thought you were my Angel.");
+System.out.println("\t\tBut \"Angel\" never lie.");
+System.out.println("\tThey don't yell and walk away, leaving you
+asking\"why\"?");
+}
+}
