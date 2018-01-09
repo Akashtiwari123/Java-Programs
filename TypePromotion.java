5c5e96478b29cf981c25be3c70a7b7f85c3b3267
Akash Tiwari <drakashti@gmail.com>
2018-01-09 20:07:40 +0530

    Create TypePromotion.java

---
+++ TypePromotion.java
@@ -0,0 +1,15 @@
+public class practice {
+
+	public static void main(String[] args) {
+	
+		byte b=42;
+		short s=1024;
+		char c='a';
+		int i=50000;
+		float f=5.6f;
+        double d=.231;
+        double result=(f*b)+(i/c)-(d*s);
+        System.out.println((f*b)+"+"+(i/c)+"-"+(d*s));
+	System.out.println("result="+result);
+	}
+}
