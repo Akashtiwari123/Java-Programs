4358c602d123ea1079f1342d4a82436d167b2c66
Akash Tiwari <drakashti@gmail.com>
2018-03-11 20:04:05 +0530

    Create HomeScreen.java

---
+++ Swing/Login_Form/HomeScreen.java
@@ -0,0 +1,22 @@
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HomeScreen extends JFrame{
+	 JPanel panel;
+	 
+	public  HomeScreen(){
+		panel=new JPanel();
+		panel.setBounds(20, 20, 300, 250);
+		panel.setLayout(null);
+		panel.setBackground(Color.ORANGE);
+	
+		add(panel);
+		setTitle("Home Screen");
+		setVisible(true);
+		setSize(600, 450);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 }
+}
