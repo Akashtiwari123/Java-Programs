542ff1232b472bbbb92f676d29785c2fc77ebb2e
Akash Tiwari <drakashti@gmail.com>
2018-03-19 19:15:07 +0530

    Create App.java
    This is the main class

---
+++ Swing/Banking_System/App.java
@@ -0,0 +1,16 @@
+package com.main;
+/*
+@author:Akash Tiwari
+*/
+public class App {
+
+	public static void main(String[] args) {
+
+		
+Customer c=new Customer("Akash",5000);
+System.out.println("The initial balance is :"+c.getBalance());
+c.deposit();
+System.out.println("The final balance is :"+c.getBalance());
+	}
+
+}
