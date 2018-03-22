9f9f524c8966beb540049d41fb0fff1ec960ac50
Akash Tiwari <drakashti@gmail.com>
2018-03-22 16:24:14 +0530

    Update App.java

--- Swing/Banking_System/com.main/App.java
+++ Swing/Banking_System/com.main/App.java
@@ -7,7 +7,7 @@ public class App {
 	public static void main(String[] args) {
 
 		
-Customer c=new Customer("Akash",5000.00;
+Customer c=new Customer("Akash",5000.00);
 System.out.println("The initial balance is :"+c.getBalance());
 c.deposit();
 System.out.println("The final balance is :"+c.getBalance());
