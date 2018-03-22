bb9246b9694d1c080f660b2d4d8d6576b69418ba
Akash Tiwari <drakashti@gmail.com>
2018-03-22 16:25:43 +0530

    Update B.java

--- Swing/Banking_System/com.pro/B.java
+++ Swing/Banking_System/com.pro/B.java
@@ -2,17 +2,19 @@ package com.pro;
 /*
 @author:Akash Tiwari
 */
+
 import com.main.Customer;
 import com.main.Withdraw;
 
 public class B implements Withdraw{
-	int balance;
+	double balance;
 	double amount;
 	
+
 	@Override
-	public void withdraw(Customer c, double amount) {
-		c.setBalance(balance=(int) (c.getBalance()-amount));
-		
+	public void withdraw(Customer c,double amount) {
+		this.balance=c.getBalance();
+		c.setBalance(this.balance=this.balance-amount);
 	}
 
 }
