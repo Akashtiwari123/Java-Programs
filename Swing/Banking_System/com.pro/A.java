ccaa1134a8178fb14bfaf8b006112cef38cc4e0f
Akash Tiwari <drakashti@gmail.com>
2018-03-25 22:43:24 +0530

    Update A.java

--- Swing/Banking_System/com.pro/A.java
+++ Swing/Banking_System/com.pro/A.java
@@ -3,24 +3,23 @@ package com.pro;
 @author:Akash Tiwari
 */
 
+
+
 import com.main.Customer;
 import com.main.Deposit;
 
 public class A implements Deposit{
 	double balance;
 	double amount;
+	Customer c;
 	
-	
+
 	@Override
 	public void deposit(Customer c, double amount) {
-		System.out.println(c.getBalance());
+	//this.c=c;
 		this.balance=c.getBalance();
-		this.amount=amount;
-		
-		c.setBalance(update());
+		this.balance=this.balance+amount;
+	    c.setBalance(balance);
 	}
-	public double update() {
-	this.balance=this.balance+amount;
-	return balance;
-	}
+	
 }
