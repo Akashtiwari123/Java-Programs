6214d0729d151f1b03bfd056b011294a5385c42b
Akash Tiwari <drakashti@gmail.com>
2018-03-28 19:23:42 +0530

    Update Customer.java

--- Swing/Banking_System/com.main/Customer.java
+++ Swing/Banking_System/com.main/Customer.java
@@ -14,13 +14,13 @@ public class Customer {
 	private String name;
 	private double balance;
 	private double amount;
-	B b=new B();
-	 A a=new A();
+	
 	public Customer(String name, double d) {
 		super();
 		this.name = name;
 		this.balance = d;
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -33,12 +33,12 @@ public class Customer {
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-	
+	/*
   void deposit() {
 	  System.out.println("Enter the amount to deposit");
 	  Scanner sc=new Scanner(System.in);
 			  this.amount=sc.nextInt();
-		       //balance=balance+amount;
+		//	  setBalance(this.balance=this.balance+amount);
 			  a.update();
 			  sc.close();
 			 
@@ -48,8 +48,7 @@ public class Customer {
 	  System.out.println("Enter the amount to withdraw");
 	  Scanner sc=new Scanner(System.in);
 			  this.amount=sc.nextInt();
-	        // balance=balance+amount;
 			B b=new B();
 			  sc.close();
-  }
+  }*/
 }
