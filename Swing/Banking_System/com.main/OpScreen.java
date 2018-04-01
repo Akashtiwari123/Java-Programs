e7a9d45e1977fda8164c08cd688e49ee309b46c9
Akash Tiwari <drakashti@gmail.com>
2018-04-01 20:14:44 +0530

    Update OpScreen.java

--- Swing/Banking_System/com.main/OpScreen.java
+++ Swing/Banking_System/com.main/OpScreen.java
@@ -1,48 +1,35 @@
 package com.main;
-
+/*
+@author:Akash Tiwari
+*/
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import com.pro.A;
-import com.pro.B;
-
 import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JRadioButton;
-import javax.swing.JInternalFrame;
-import java.awt.Panel;
-import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JSeparator;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class OpScreen extends JFrame {
+public class MainScreen extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPanel panel;
-	
+	private JTextField txtname;
+	private JTextField txtbalance;
 	private String name;
 	private double balance;
-	
 
+	
+	
 	/**
 	 * Create the frame.
-	 * @param c 
 	 */
-	public OpScreen(Customer c) {
+	public MainScreen() {
 		
-		name=c.getName();
-		System.out.println(name);
-		balance=c.getBalance();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setVisible(true);
@@ -51,57 +38,55 @@ public class OpScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblWelcome = new JLabel("Welcome");
-		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblWelcome.setBounds(46, 25, 86, 22);
-		contentPane.add(lblWelcome);
 		
-		JLabel lblYourBalance = new JLabel("Your Balance");
-		lblYourBalance.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblYourBalance.setBounds(46, 58, 86, 22);
-		contentPane.add(lblYourBalance);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setText(name);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel.setBounds(183, 27, 125, 21);
-		contentPane.add(lblNewLabel);
+		JLabel lblWelcomeBankSystem = new JLabel("Welcome  Bank System");
+		lblWelcomeBankSystem.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblWelcomeBankSystem.setBounds(99, 28, 223, 26);
+		contentPane.add(lblWelcomeBankSystem);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		String bal=String.valueOf(balance);
-		lblNewLabel_1.setText(bal);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(183, 60, 125, 21);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblCustomreName = new JLabel("Customer Name");
+		lblCustomreName.setFont(new Font("Verdana", Font.PLAIN, 14));
+		lblCustomreName.setBounds(40, 88, 113, 26);
+		contentPane.add(lblCustomreName);
 		
-		 ButtonGroup bg=new ButtonGroup();
+		JLabel lblInitialBalance = new JLabel("Initial Balance");
+		lblInitialBalance.setFont(new Font("Verdana", Font.PLAIN, 14));
+		lblInitialBalance.setBounds(40, 142, 113, 26);
+		contentPane.add(lblInitialBalance);
 		
-		JRadioButton rdbtnDeposit = new JRadioButton("Deposit");
-		rdbtnDeposit.addActionListener(new ActionListener() {
+		txtname = new JTextField();
+		txtname.setBounds(184, 93, 138, 20);
+		contentPane.add(txtname);
+		txtname.setColumns(10);
+		
+		txtbalance = new JTextField();
+		txtbalance.setBounds(184, 147, 138, 20);
+		contentPane.add(txtbalance);
+		txtbalance.setColumns(10);
+		
+		 
+		 JButton btnProcess = new JButton("Process");
+		btnProcess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				panel.setVisible(true);
+				
+				name=txtname.getText().toString();
+				 balance=Double.parseDouble(txtbalance.getText().toString());
+				 
+				 Customer c=new Customer(name,balance);
+				 OpScreen op=new OpScreen(c);
+
+				 op=null;
+				 
 			}
 		});
-		rdbtnDeposit.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rdbtnDeposit.setBounds(46, 104, 109, 23);
-		contentPane.add(rdbtnDeposit);
+		btnProcess.setFont(new Font("Verdana", Font.PLAIN, 14));
+		btnProcess.setBounds(139, 199, 99, 27);
+		contentPane.add(btnProcess);
 		
-		JRadioButton rdbtnWithdraw = new JRadioButton("Withdraw");
-		rdbtnWithdraw.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panel.setVisible(true);
-			}
-		});
-		rdbtnWithdraw.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rdbtnWithdraw.setBounds(46, 130, 109, 23);
-		contentPane.add(rdbtnWithdraw);
-		bg.add(rdbtnDeposit);
-		bg.add(rdbtnWithdraw);
 		
-	    panel= new JPanel();
-		panel.setBounds(46, 180, 318, 44);
-		contentPane.add(panel);
-		panel.setVisible(false);
-    
-    }
-    }
+	}
+
+	
+	
+}
