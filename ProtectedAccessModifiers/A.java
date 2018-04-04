9269aada327f6d3325f2ad1e197afc1bceafc3c3
Akash Tiwari <drakashti@gmail.com>
2018-04-04 20:52:43 +0530

    Rename App.java to A.java

---
+++ ProtectedAccessModifiers/A.java
@@ -0,0 +1,18 @@
+package p1;
+/*
+Author:Akash Tiwari
+*/
+import java.util.Scanner;
+
+import p2.B;
+
+
+public class App extends B{
+	public static void main(String[] args) {
+		App a=new App();
+		System.out.println(a.x);
+		a.m1();
+	 
+	}
+	
+}
