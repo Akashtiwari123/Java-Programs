ff017ce6a7afadaca2196d09db8effe129aa8590
Akash Tiwari <drakashti@gmail.com>
2018-04-06 19:20:30 +0530

    Create AnonymousAndStaticBLocks.java
    Anonymous and Static blocks

---
+++ AnonymousAndStaticBLocks.java
@@ -0,0 +1,22 @@
+
+class A{
+	//Anonymous blocks
+	{        //Called when object is created.
+		System.out.println("Welcome");
+	}
+	
+	//Static blocks
+	static {    //Called when class is loaded in memory
+		System.out.println("Thanks!");
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		  A a=new A();
+		  
+
+	}
+
+}
