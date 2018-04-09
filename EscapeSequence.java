157b7a19157bd8823754dc3993cdb8870a82053b
Akash Tiwari <drakashti@gmail.com>
2018-04-09 23:19:34 +0530

    Update EscapeSequence.java

--- EscapeSequence.java
+++ EscapeSequence.java
@@ -2,7 +2,7 @@
 statements.
 SOURCE CODE:*/
 import java.io.*;
-public class Harry
+public class App
 {
 public static void main(String args[])
 {
@@ -11,7 +11,6 @@ System.out.println("\t\tBut Angel never lie.");
 System.out.println("\t\t\"\\\\//\\\\//\"");
 System.out.println("\tI thought you were my Angel.");
 System.out.println("\t\tBut \"Angel\" never lie.");
-System.out.println("\tThey don't yell and walk away, leaving you
-asking\"why\"?");
+System.out.println("\tThey don't yell and walk away, leaving you asking\"why\"?");
 }
 }
